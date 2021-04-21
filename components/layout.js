434339034897b66eba7bee9923863883f1d43948
aliabdulazeez <aliabdulazeez01@gmail.com>
2021-04-21 15:39:32 +0900

    title changed

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,8 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Adam Nomanoff";
-export const siteTitle = "Next.js Sample Website";
+
+export const siteTitle = "Nomanoff's Blogs";
 
 export default function Layout({ children, home }) {
   return (
